e3eebb756bd18656bf3c98c6d1d2d6072eefc339
pleaseddd <147441873+pleaseddd@users.noreply.github.com>
2024-02-25 17:30:07 +0100

    Add files via upload

---
+++ tetramino.cpp
@@ -0,0 +1,71 @@
+#include "tetramino.h"
+#include "tetramini.h"
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+Tetramino::Tetramino() {
+    srand(time(NULL));
+    tipo=rand()%6;
+    rotazione=rand()%3;
+    aggiornaTavola();
+}
+
+void Tetramino::aggiornaTavola() {
+    for (int i = 0; i < 20 + 4; ++i) {
+        for (int j = 0; j < 10; ++j) {
+            if (i - y < 4 && i - y >= 0 &&
+                j - x < 4 && j - x >= 0) {
+                tavola[i][j] = tetramini[tipo][rotazione][i - y][j - x];
+            } else {
+                tavola[i][j] = 0;
+            }
+        }
+    }
+}
+bool Tetramino::collisioniBordo() {
+    aggiornaTavola();
+    int blocco=0;
+    for (int i = 0; i < 20 + 4; ++i) {
+        for (int j = 0; j < 10; ++j) {
+            if (tavola[i][j] != 0) blocco += 1;
+        }
+    }
+    if (blocco < 4) return true;
+    return false;
+}
+bool Tetramino::destra() {
+    x=x+1;
+    if (collisioniBordo()){
+        x=x-1;
+        return false;
+    }
+    return false;
+}
+bool Tetramino::sinistra() {
+    x=x-1;
+    if (collisioniBordo()){
+        x=x+1;
+        return false;
+    }
+    return false;
+}
+bool Tetramino::giu() {
+    y=y-1;
+    if (collisioniBordo()){
+        y=y+1;
+        return false;
+    }
+    return false;
+}
+bool Tetramino::ruota(bool reverse) {
+    if (reverse){
+        rotazione=rotazione+2;
+    }
+    rotazione=(rotazione+1)%4;
+    if (collisioniBordo()){
+        rotazione=(rotazione+3)%4;
+        return false;
+    }
+    return true;
+}
---
+++ tetramino.h
@@ -0,0 +1,21 @@
+#ifndef TETRIS_TETRAMINO_H
+#define TETRIS_TETRAMINO_H
+
+class Tetramino{
+public:
+    Tetramino();
+    void aggiornaTavola();
+    bool destra();
+    bool sinistra();
+    bool giu();
+    bool ruota(bool =false);
+    int tavola[24][10]={};
+private:
+    int y=1;
+    int x=3;
+    int rotazione;
+    int tipo;
+    bool collisioniBordo();
+};
+
+#endif //TETRIS_TETRAMINO_H
