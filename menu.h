c8e29ae0c8817d69d5dd762601a5b327a2c95ff2
bies0 <161234928+bies0@users.noreply.github.com>
2024-03-17 09:46:02 +0100

    Add files via upload

--- menu.h
+++ menu.h
@@ -2,6 +2,7 @@
 #define MENU_H
 #include <cstring>
 #include <fstream>
+using namespace std;
 
 class mainMenu{
     public:
@@ -78,14 +79,104 @@ class mainMenu{
                 }
             }
         }
-
+        // stampa il menu dei punteggi
         void scoreboardMenu() {
             initialize();
-            //codice qui
+            char linea[64];
+            char nome[32];
+            char punti[10];
+            getmaxyx(stdscr, yMax, xMax);
+            WINDOW *win = newwin(yMax, xMax, 0, 0);
+            refresh();
+            box(win, 0, 0);
+            curs_set(0);
+            fstream file;
+            file.open ("classifica.txt",ios::in);
+
+            if (file.is_open()) {
+                if(controllo_file()) {
+                    int ind = 0;
+                    // scorre il file riga per riga
+                    while (!file.eof() && ind < yMax-4) {
+                        file.getline(linea, 32);
+                        //stampa il nome e il punteggio salvati nella riga
+                        if (linea[0] != '\0') {
+                            ricava_nome_punti(linea, punti, nome);
+
+                            mvwprintw(win, ind + 2, 3, "%d >>", ind + 1);
+                            mvwprintw(win, ind + 2, 10, "%s", nome);
+                            mvwprintw(win, ind + 2, 28, "%s", punti);
+                            ind++;
+                        }
+                    }
+                }
+                else {
+                    mvwprintw(win, 2, 2, "file classifica.txt manomesso");
+                }
+                file.close();
+            }
+            wrefresh(win);
+            wgetch(win);
             mainMenu();
         }
+
     protected:
     int yMax, xMax;
+
+        // estrae il nome e il punteggio da una riga
+    void ricava_nome_punti(char linea[], char punti[], char nome[]){
+        int x = 0;
+        if(linea[0] != '\0') {
+            while (linea[x] != ';') {
+                punti[x] = linea[x];
+                x++;
+            }
+            punti[x] = '\0';
+
+            int y = 0;
+            while (linea[x + y + 1] != ';' && y < 20) {
+                nome[y] = linea[x + y + 1];
+                y++;
+            }
+            nome[y] = '\0';
+        }
+        else {
+            nome[0] = '\0';
+            punti[0] = '\0';
+        }
+    }
+
+    // controlla il formato delle righe nel file classifica.txt
+    bool controllo_file(){
+        fstream file;
+        file.open("classifica.txt", ios::in);
+
+        char linea[32];
+        while(!file.eof()) {
+            file.getline(linea, 32);
+            if (linea[0] != '\0') {
+                int len = strlen(linea);
+                if (len > 26) return false;
+                //controlla che il punteggio non sia troppo lungo e che ci siano solo cifre
+                int x = 0;
+                while (linea[x] != ';' ){
+                    if (linea[x] == '\0') return false;
+                    if (x >= 9) return false;
+                    if (linea[x] < '0' || linea[x] > '9') return false;
+                    x++;
+                }
+                // controlla che il nome non sia troppo lungo
+                int y = 1;
+                while (linea[x + y] != ';'){
+                    if (linea[x + y] == '\0') return false;
+                    if (y > 15) return false;
+                    y++;
+                }
+            }
+        }
+        file.close();
+        return true;
+    }
 };
 
 #endif
